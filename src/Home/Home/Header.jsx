ebebacaa9a3957d09f9bc704cd7fd41badb4290f
saidur-way <saiduronway@gmail.com>
2023-05-02 13:15:13 +0600

    banner added

--- src/Home/Home/Header.jsx
+++ src/Home/Home/Header.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
      return (
-          <div>
-               <h2>This is header!</h2>
-          </div>
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container className='fs-5'>
+        <Navbar.Brand href="#home">Food Idea</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mx-auto">
+            <Link className='px-4 text-decoration-none text-white' to="/">Home</Link>
+            <Link className='px-4 text-decoration-none text-white' to="/About">About</Link>
+          </Nav>
+          <Nav className=''>
+            <Link className='px-4 text-decoration-none text-white' to="">More deets</Link>
+           <Button ><Link className=' text-decoration-none text-white' to=''>Login</Link></Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
      );
 };
 
--- src/Home/Home/Home.jsx
+++ src/Home/Home/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import bgBanner from '../../assets/bg-banner.jpg'
+import banner from '../../assets/food-banner.jpg'
+import chefImg from '../../assets/pngwing.com (6).png'
 
 const Home = () => {
-     return (
-          <div>
-               <h1>This is Home page!</h1>
-          </div>
-     );
-};
+  return (
+    <div>
+      <h1>This is header!</h1>
+    </div>
+  )
+}
 
-export default Home;
+export default Home
